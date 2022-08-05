a4351d06e52c9dce0a586f0c001e3d3b3e1a1232
MatiasPesobas <110672045+MatiasPesobas@users.noreply.github.com>
2022-08-05 10:27:30 -0300

    Add files via upload

---
+++ Frontend/registro.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+ <head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale-1.0">
+  <title>Document</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+  <div class="formulario">
+    <h1>Registro</h1>
+    <form method="post">
+      <div class="username">
+        <input type="text" required>
+        <label>DNI</label>
+</div> 
+<div class="contraseña">
+  <input type="password" required>
+  <label>Contraseña</label>
+</div>
+<input type= "submit" value="Registrarse">
+<div class="registrarse">
+  ¿Ya tengo una cuenta?  <a href ="./proyecto.php">Iniciar Sesion</a>
+</div>
+</form>
+
+  <div> 
+</body>
+</html>
