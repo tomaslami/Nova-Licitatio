65f474293afeb55323d88544ee646c26428540cf
tomaslami <101568838+tomaslami@users.noreply.github.com>
2022-09-14 14:32:05 -0300

    Update prueba.sol

--- Blockchain/prueba.sol
+++ Blockchain/prueba.sol
@@ -1,23 +1,26 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity ^0.4.19;
+pragma solidity ^0.8.7;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract Proyecto {
 
 struct Licitacion {
+address OwnerComp;
 uint ProcessNum;
 string ProcessName;
 string Ubi;
 uint SuggAmount;
 uint Time;
     }
-event NewLicit (address licit_Id, uint processNum, uint suggAmount, string ubi, string processName, uint time);
+event NewLicit (address OwnerComp, uint processNum, uint suggAmount, string ubi, string processName, uint time);
 Licitacion[] public licitaciones;
 mapping(uint => address) public Comp_Licit;
 mapping(uint => address) public Comp_Cons;
 
-function CreateLicit(uint _ProcessNum, uint _SuggAmount, uint _Time, string memory _Ubi, string memory _ProcessName) internal {
-    uint Id = licitaciones.push(Licitacion(_ProcessNum, _ProcessName, _Ubi, _SuggAmount, _Time));
+function CreateLicit(address _OwnerComp, uint _ProcessNum, uint _SuggAmount, uint _Time, string memory _Ubi, string memory _ProcessName) public {
+    uint Id = licitaciones.push(Licitacion(_OwnerComp, _ProcessNum, _SuggAmount, _Time, _Ubi, _ProcessName));
     Comp_Licit[Id] = msg.sender;
     Comp_Cons[Id] = msg.sender;
     NewLicit(msg.sender, _ProcessNum, _SuggAmount, _Ubi, _ProcessName, _Time);
@@ -28,7 +31,4 @@ modifier onlyComp (uint _LicitId){
     _;
 }
 
-function getLicitByOwner (address _Owner) internal view returns(int[]) {
-    
 }
-
